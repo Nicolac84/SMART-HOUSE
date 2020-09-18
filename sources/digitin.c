6ffbbd1cf0c3ca0e54f816570f159b793060946f
Nicolac84 <64138107+Nicolac84@users.noreply.github.com>
2020-09-18 15:29:36 +0200

    digital input

---
+++ sources/digitin.c
@@ -0,0 +1,12 @@
+#include <avr/io.h>
+#include "digitin.h"
+
+void digit_init (void) {
+  DDRC = 0xFF;
+  PORTC = 0xFF;
+}
+
+uint8_t digit_read (uint8_t pin) {
+  if (pin > PIN30) return 2;
+  return (PINC & (1 << pin)) >> pin;
+}
